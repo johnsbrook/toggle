34c1a0e7b2005962ec03110f09abaf5ba2ba0560
Ivan Zapata <falenyo@gmail.com>
2020-10-05 23:40:07 -0400

    styled button

--- js/onlick.js
+++ js/onlick.js
@@ -1,32 +1,30 @@
 function onClick() {
+  start();
 
-    start();
+  var val = true;
 
-    var val = true;
+  function start() {
+    document.body.addEventListener("click", function (event) {
+      if (event.target.className == "toggle bg-info") {
+        valToggle();
+      }
+    });
+  }
 
-    function start() {
-        document.body.addEventListener("click", function (event) {
-            if (event.target.className == "toggle") {
-                valToggle();
-            }
-        });
+  function valToggle() {
+    console.log("Before: ", val);
+    val = !val;
+    console.log("After: ", val);
+    backgroundColor();
+  }
+
+  function backgroundColor() {
+    if (val) {
+      document.body.setAttribute("class", " ");
+    } else {
+      document.body.setAttribute("class", "bg-danger");
     }
-
-    function valToggle() {
-        console.log("Before: ", val);
-        val = !val;
-        console.log("After: ", val);
-        backgroundColor();
-    }
-
-    function backgroundColor() {
-        if (val) {
-            document.body.setAttribute("class", " ");
-        } else {
-            document.body.setAttribute("class", "bg-danger")
-        }
-    }
-
+  }
 }
 
 export default onClick();
