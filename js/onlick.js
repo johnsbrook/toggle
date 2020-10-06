be67f5a1e884a6945a61831fa8cb4c2aa72361ec
Ivan Zapata <falenyo@gmail.com>
2020-10-06 01:28:05 -0400

    deleted changeBackground() function

--- js/onlick.js
+++ js/onlick.js
@@ -20,15 +20,6 @@ function onClick() {
     console.log("Before: ", val);
     val = !val;
     console.log("After: ", val);
-    backgroundColor();
-  }
-
-  function backgroundColor() {
-    if (val) {
-      document.body.setAttribute("class", " ");
-    } else {
-      document.body.setAttribute("class", "bg-danger");
-    }
   }
 
   function changeValue(event) {
--- js/script.js
+++ js/script.js
@@ -1,3 +1,2 @@
 import onClick from './onlick.js';
 
-
