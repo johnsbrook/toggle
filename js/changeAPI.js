14eb676e2a1aa37a661294a3673e9dc7d8ec7007
Ivan Zapata <falenyo@gmail.com>
2020-10-06 01:23:37 -0400

    toggling between tea and coffee working

--- js/changeAPI.js
+++ js/changeAPI.js
@@ -2,6 +2,7 @@ function changeAPI() {
   first();
   var val = true;
   var API;
+  const promo = document.querySelector("#display");
 
   function first() {
     document.body.addEventListener("click", function (event) {
@@ -21,25 +22,59 @@ function changeAPI() {
   }
 
   function callAPI(API) {
-
+    
     const createNode = (elem) => {
       return document.createElement(elem);
     };
     const appendNode = (parent, elem) => {
-      // console.log(elem)
       parent.appendChild(elem);
     };
-
     var idNo = 0;
+    promo.innerHTML = ''
+    products();
 
+    function products() {
+   
     fetch(API)
       .then((res) => res.json())
       .then((d) => {
           d.map((d) => {
-              console.log("This is d mapped", d.product)
+            let li = createNode("li");
+            let btn = createNode("button");
+            let btn2 = createNode("button");
+            let br = createNode("br");
+            let spanH = createNode("div");
+            let span = createNode("span");
+            let p = createNode("div");
+            let image = createNode("img");
+  
+  
+            spanH.innerText = d.product + " " + " $" + d.price;
+            spanH.id = d._id;
+            spanH.setAttribute("class", "add");
+            p.innerText = d.description;
+            (btn.innerText = "Add"), btn.setAttribute("class", "add");
+            btn.setAttribute("id", d.category + (idNo += 1));
+            btn2.innerText = "More";
+            image.setAttribute("src", d.image);
+            image.setAttribute(
+              "style",
+              "width: 150px; height: auto; border-radius: 15px;"
+            );
+  
+            appendNode(li, image);
+            appendNode(li, spanH);
+            appendNode(li, span);
+            appendNode(li, p);
+            appendNode(li, btn);
+            appendNode(li, btn2);
+            appendNode(promo, li);
           })
+      
+        
       });
   }
+}
 }
 
 export default changeAPI();
